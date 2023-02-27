0ce17781a64b2b59793893cd7b9810be27571203
helgibrandt <OlegBulbaDev@gmail.com>
2023-02-27 20:47:44 +0200

    Stuff used in main

--- MatrixSrc/main.c
+++ MatrixSrc/main.c
@@ -1,3 +1,27 @@
+#include <stdio.h>
+#include "Input.h"
+#include "Output.h"
+#include "ErrorCodes.h"
 
 int main() {
+
+    int successToken;
+
+    FILE* inputData = GetFile(&successToken);
+    if (successToken != 0)
+        return successToken;
+
+    ReadDimensions(inputData, &successToken);
+    if (successToken != 0)
+        return successToken;
+
+    ReadMatrix(inputData, &successToken);
+    if (successToken != 0)
+        return successToken;
+
+    OutputMatrix();
+
+    return 0;
 }
+
+
